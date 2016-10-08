d326c966a5347468defa01af66ebcb0386725949
Nikush Dalia <nad71@pitt.edu>
2016-10-08 06:29:18 -0400

    First push

--- gm/src/js/app.js
+++ gm/src/js/app.js
@@ -1,45 +1,37 @@
 // Your code goes here
+$(document).ready(function() {
+    
+    //Teen Active Alerter
+    function showActive(data) {
+        if (data.teen_driver_active == '$1') {
+            $.post("https://maker.ifttt.com/trigger/active_teen/with/key/d-_zxVjZpr34awx2JXkRXM");
+            gm.info.getCurrentPosition(success, true);
+        }
+    }
+    
+    gm.info.watchVehicleData(showActive, ['teen_driver_active']);
 
+    
+    // Longitude and Latitude
+    function success(position) {
+      var long = position.coords.longitude;
+      var lati = position.coords.latitude;
+      if ((long != undefined) && (lati != undefined)) {
+          var longText = document.getElementById('long');
+          var latiText = document.getElementById('lati');
+          longText.innerHTML = long;
+          latiText.innerHTML = lati;
+      }
+    }
 
-function processData(data) {
-  console.log('got vehicle data: ', data);
-  if (data.teen_drowsy_alerts) {
-    console.log(data.teen_drowsy_alerts);
-  }
-}
-
-function showSpeed(data) {
-  console.log(data);
-  var speed = data.average_speed;
-  if (speed !== undefined) {
-    var speedText = document.getElementById('speed');
-    speedText.innerHTML = speed;
-  }
-};
-
-$(document).ready(function(){
-
-
-gm.info.watchVehicleData(showSpeed, ['average_speed']);
-gm.info.getVehicleData(showSpeed, ['average_speed']);
-
-gm.ui.showAlert({
-  alertTitle: 'Hey Jude',
-  alertDetail: 'Don\'t let me down',
-  primaryButtonText: 'I won\t!',
-  primaryAction: function stayAndPractice() {},
-  secondaryButtonText: 'Sorry, Paul',
-  secondaryAction: function hangWithYoko() {}
-});
-
-
-
-// Call processData will all available signals. Expect a 5+ second delay before callback is triggered
-gm.info.getVehicleData(processData);
-
-// Call processData with only 'engine_oil_temp' signal. Callback triggered much faster with fewer signals
-gm.info.getVehicleData(processData, ['teen_drowsy_alerts','airbag_deployed']);
-
-
-
+    gm.info.watchPosition(success, true);
+    
+    // Speed
+    /*function showSpeed(data) {
+        var speed = data.average_speed;
+        if (speed != undefined) {
+            var speedText = document.getElementById('speed');
+            speedText.innerHTML = speed;
+        }
+    }*/
 });
